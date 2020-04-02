0a38eca2b3d4138135e0b104379c6b8fad5f9646
jack-sykes <61171002+jack-sykes@users.noreply.github.com>
2020-04-02 18:26:47 -0400

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-  res.render('helloworld', { title: 'Hello, World!' });
+  res.render('index', { title: 'Hello, World!' });
 });
 
 /* GET Userlist page. */
